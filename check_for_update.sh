eab2fac3d09b6389437b379ad1fdf7724aeb111b
Ian Sohl <docsohl@gmail.com>
2021-05-03 15:34:53 -0700

    Update update checker script

--- check_for_update.sh
+++ check_for_update.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
 set -ex
-pushd ~/MSApublic
+pushd /home/pi/MSApublic
 git remote update
 CURRENT_STATUS=$(git status -uno)
 if [[ $CURRENT_STATUS == *"is behind"* ]]; then
+  echo "Downloading new version"
+  systemctl stop MSA
   git pull
-  systemctl restart MSA
+  cmp --silent /home/pi/build/WEB_VERSION WEB_VERSION && echo "No web build change" || echo "Downloading new web build" && aws s3 cp "$(cat WEB_VERSION)" /tmp/web_build.zip && rm -r /home/pi/build && unzip /tmp/web_build.zip -d /home/pi/
+  systemctl start MSA
 fi
